cc6096a35aa18ee4dc6ac570fc66ce0a8b9b43b5
LucaBernardinii <luca.bernardini@studenti.isissgobetti.it>
2024-10-01 15:15:24 +0200

    esercizio 6

--- bernardini_es006.py
+++ bernardini_es006.py
@@ -1,28 +1,32 @@
 class Pagamento:
 
-    def __init__(self, processa_pagamento):
-        self.processa_pagamento = processa_pagamento
-
+    def processa_pagamento():
+        return "Pagamento."
 
 class CartaDiCredito(Pagamento):
+    def __init__(self,nome, conto, data, cvv):
+        self.nome = nome
+        self.conto = conto
+        self.data = data
+        self.cvv = cvv
 
-    def __init__(self, processa_pagamento):
-        super().__init__(processa_pagamento)
-        self.processa_pagamento = processa_pagamento
+    def processa_pagamento(self):
+        return f"Pagamento con Carta di Credito effettuato da {self.nome}."
 
 class PayPal(Pagamento):
 
-    def __init__(self, processa_pagamento):
-        super().__init__(processa_pagamento)
-        self.processa_pagamento = processa_pagamento
+    def __init__(self,email, password):
+        self.email = email
+        self.password = password
 
+    def processa_pagamento(self):
+        return f"Pagamento con PayPal per {self.email}."
 
 def effettua_pagamento(metodo_di_pagamento: Pagamento):
-    metodo_di_pagamento.processa_pagamento()
+    print(metodo_di_pagamento.processa_pagamento())
 
 pagamento_carta = CartaDiCredito("Mario Rossi", "1234 5678 9012 3456", "12/23", "123")
 pagamento_paypal = PayPal("mario.rossi@example.com", "password123")
 
-effettua_pagamento(pagamento_carta)  
-effettua_pagamento(pagamento_paypal)  
-
+effettua_pagamento(pagamento_carta)
+effettua_pagamento(pagamento_paypal)
